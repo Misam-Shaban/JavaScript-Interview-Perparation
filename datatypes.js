5705d8e7a574074b73441cb3766babd0c40829dd
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-22 19:07:50 +0500

    All Primitive Data Type

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,23 @@
 // DATA types in javascript
+// In javascript we have 2 types of data types primitive data types
+// and non primitive data types
+
+// Primitive Data Types
+console.log("Primitive Data Types");
+
+let a = 10;  
+console.log(typeof(a)); // Number Data types
+let b = true; // Boolean Data types
+console.log(typeof(b)); // Boolean Data types
+let c = "Misam"; // String Data types
+console.log(typeof(c)); // String Data types
+let d;
+console.log(typeof(d)); // Undefined Data types
+let e = null;
+console.log(typeof(e)); // Null Data types
+let f = 123467578686n; 
+console.log(typeof(f)); // Bigint Data types
+let g = Symbol("#");
+console.log(typeof(g)); // Symbol Data types
+console.log(g);
+
