5c43972a8e498de0ab981a25c2782e8aaed085a9
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-22 18:22:15 +0500

    Var function Scope

--- Diff_Var_let_const.js
+++ Diff_Var_let_const.js
@@ -1,5 +1,24 @@
-if(true){
-var a = 10;
-let b = 20;
-const c = 30;
+// if(true){
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// }
+
+// console.log(a);
+
+// OUTPUT:  10 ku k var ka scope block nh hota functional hota ha 
+
+function scope(){
+    var a = 50;
 }
+
+console.log(a);
+
+
+// console.log(b);
+// console.log(c);
+
+// outPut: Error ku k let or const ka scope block hota ha 
