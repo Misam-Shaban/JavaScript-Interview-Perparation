4ef6ebe2c3fd0ec4b45dc145ac15184d6fc0722f
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-22 17:45:03 +0500

    Arrow Funtion Hoisting

--- variable_hoisting.js
+++ variable_hoisting.js
@@ -1,19 +1,52 @@
-// console.log(a);
-
-// let a = 5;
-
-// console.log(a);
 
 
-let a = 10; 
-a = 20;
+// What is Hoisting?
+
+// Hoisting is Js behavior where all variable and function
+// declaration move at the top of their scope During memory Creation Phase...
+
+
+
+// console.log(b);
+// // var b = 10;
+
+// output: This will give You undefined Due to Hoisting
+
+
+// console.log(a); 
+// let a = 10; 
+// a = 20;
+
+// output: This will give You Error: Variable not define Due to Hoisting
+
+
+// console.log(x); 
+
+// const x = 15; 
+
+// console.log(x);
+
+// output: This will give You Error: Variable not define Due to Hoisting
 
 
 greet();
 
+// console.log(a);
+
 function greet(){
-    console.log("Hello!");
+    console.log("Function Run Due to Hoisting");
 };
+
+// greet();
+
+
+// Hoisting Arrow Function Work nh krti
+
+greet1();
+
+const greet1 = ()=>{
+  console.log("Hi..");
+}
 
 
 
