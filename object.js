0468936685e310936e34c6fecc9c012c780c53d9
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-25 16:44:29 +0500

    Practice

--- object.js
+++ object.js
@@ -1,5 +1,9 @@
-obj = {
-    name: "Misam",
+const obj = {
+    fname: "Misam",
+    lname: "Shaban",
     age: 24,
-    city: "Sahiwal"
+    city: "Sahiwal",
+    getFullname(){
+        return `My Full Name is ${this.name} ${this.lname} `
+    }
 };
