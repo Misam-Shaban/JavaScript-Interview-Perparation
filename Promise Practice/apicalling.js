9d30dc10b051bee7205d112c75cf16a8af82726e
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-26 20:58:24 +0500

    Practice

--- Promise Practice/apicalling.js
+++ Promise Practice/apicalling.js
@@ -1,11 +1,16 @@
 function getJoke() {
-  fetch("https://official-joke-api.appspot.com/")
-    .then((respones) => {
-      return respones.json();
-    }).then((newJoke)=>{
-        console.log(newJoke);
+  fetch("https://v2.jokeapi.dev/joke/Any")
+    .then((response) => { // 'respones' ko 'response' kar diya
+      return response.json();
+    })
+    .then((newJoke) => {
+      console.log("data mil gya...");
+      console.log(newJoke.joke);
     })
     .catch((error) => {
       console.log("Data nh mila...");
+      console.log(error); // Error dekhne ke liye isko add karna behtar hai
     });
 }
+
+getJoke();
