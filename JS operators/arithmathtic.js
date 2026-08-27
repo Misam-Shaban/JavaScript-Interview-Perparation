fdf6c75505e36a14890ad40b9501b59244929156
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-27 16:57:57 +0500

    Practice

--- JS operators/arithmathtic.js
+++ JS operators/arithmathtic.js
@@ -1,1 +1,12 @@
-console.log(4 % 10); // output 4
+// console.log(4 % 10); // output 4
+// console.log( 10 % 4); // output 4
+
+// let x = 5;
+// x--;
+// let z = x;
+// console.log(z);
+
+
+// console.log(x**2);
+
+
