6fb1bfb0b3ade3766d40f972cb86d2474fe8014e
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-27 20:08:06 +0500

    REST & Spread

--- JS operators/arithmathtic.js
+++ JS operators/arithmathtic.js
@@ -1,3 +1,4 @@
+// "use strict"
 // console.log(4 % 10); // output 4
 // console.log( 10 % 4); // output 4
 
@@ -8,5 +9,32 @@
 
 
 // console.log(x**2);
+
+
+// x = 5;
+
+// console.log(x);
+
+// let age = 20;
+// let hasCnic = false;
+
+
+// console.log(age >= 18 && hasCnic);
+// console.log(age < 18 || hasCnic);
+
+
+// const arr = [1,2,3];
+// console.log(...arr);
+
+// const str = "MisamShaban";
+
+// console.log(...str);
+
+let func = ((...data)=>{
+    console.log(data);
+})
+
+func(1,2,3,4,5);
+
 
 
