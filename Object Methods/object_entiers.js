a506fed3b010169f264f837ec84f8122857ffcc7
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-28 21:32:09 +0500

    Object Entiers Practice

--- Object Methods/object_entiers.js
+++ Object Methods/object_entiers.js
@@ -1,41 +1,59 @@
-const student = {
+// const student = {
+//   name: "Misam",
+//   age: 23,
+//   course: "BSCS"
+// };
+
+// const entries = Object.entries(student);
+
+// console.log(entries);
+
+
+// for (const arrOn of entries ){
+//     console.log("arr",arrOn);
+// }
+
+// // Without destructuring
+
+// for( const entry of entries ){
+//     console.log(entry[0], ":", entry[1] );
+    
+// }
+
+// // Destructuring
+
+// for (const [key,value] of entries ){
+//     console.log(key, ":" , value);
+// }
+
+// console.log("      ");
+
+
+// const product = {
+//   name: "Laptop",
+//   price: 100000,
+//   brand: "HP",
+//   stock: 10
+// };
+
+// for (const [key, value] of Object.entries(product)) {
+//   console.log(`${key}: ${value}`);
+// }
+
+
+
+
+const employee = {
   name: "Misam",
   age: 23,
-  course: "BSCS"
+  position: "Frontend Developer",
+  city: "Sahiwal"
 };
 
-const entries = Object.entries(student);
 
-console.log(entries);
+const employeeEntries =  Object.entries(employee);
 
-
-for (const arrOn of entries ){
-    console.log("arr",arrOn);
-}
-
-// Without destructuring
-
-for( const entry of entries ){
-    console.log(entry[0], ":", entry[1] );
-    
-}
-
-// Destructuring
-
-for (const [key,value] of entries ){
-    console.log(key, ":" , value);
-}
-
-console.log("      ");
+console.log(employeeEntries);
+console.log(employeeEntries.length);
 
 
-const product = {
-  name: "Laptop",
-  price: 100000,
-  brand: "HP",
-  stock: 10
-};
-
-for (const [key, value] of Object.entries(product)) {
-  console.log(`${key}: ${value}`);
-}
