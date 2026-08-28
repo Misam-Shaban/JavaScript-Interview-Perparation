5bdfebece786ec109157c65f7d47ae6bf589f254
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-28 22:23:59 +0500

    Practice

--- Object Methods/object_assign.js
+++ Object Methods/object_assign.js
@@ -1,1 +1,24 @@
-// Object.assign() ek ya multiple source objects ki properties ko target object mein copy karta hai.
+// Simple Definition:
+// Object.assign() ek ya multiple source objects ki 
+// properties ko target object mein copy karta hai.
+
+// Rule:
+// Agar same property multiple sources mein ho, to baad 
+// mein aane wali value previous value ko overwrite kar deti hai.
+
+
+const target = {
+  name: "Misam"
+};
+
+console.log(target);
+
+const source = {
+  age: 23,
+  city: "Sahiwal"
+};
+
+
+const newObj = Object.assign(target,source)
+console.log("T","",target);
+console.log(newObj);
