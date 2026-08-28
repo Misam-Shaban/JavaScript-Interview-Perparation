4124d2266cc21dcc9d598c05ae6f5e9c92b4a202
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-28 21:27:22 +0500

    Practice

---
+++ Object Methods/object_entiers.js
@@ -0,0 +1,41 @@
+const student = {
+  name: "Misam",
+  age: 23,
+  course: "BSCS"
+};
+
+const entries = Object.entries(student);
+
+console.log(entries);
+
+
+for (const arrOn of entries ){
+    console.log("arr",arrOn);
+}
+
+// Without destructuring
+
+for( const entry of entries ){
+    console.log(entry[0], ":", entry[1] );
+    
+}
+
+// Destructuring
+
+for (const [key,value] of entries ){
+    console.log(key, ":" , value);
+}
+
+console.log("      ");
+
+
+const product = {
+  name: "Laptop",
+  price: 100000,
+  brand: "HP",
+  stock: 10
+};
+
+for (const [key, value] of Object.entries(product)) {
+  console.log(`${key}: ${value}`);
+}
