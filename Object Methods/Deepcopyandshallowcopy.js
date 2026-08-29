b96b5adba14ef3a84a6e2005fb7e9a0ec80ad230
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-29 22:37:41 +0500

    Practice

--- Object Methods/Deepcopyandshallowcopy.js
+++ Object Methods/Deepcopyandshallowcopy.js
@@ -12,4 +12,25 @@ const userName1 = "Misam";
 
 let userName2 = userName1;
 
+userName2 = "Admad";
  
+console.log(userName2);
+
+
+const obj1 = {
+     age: 24,
+    city: "Sahiwal",
+    name:"Misam Shaban"
+};
+
+
+const obj2 = {}
+
+
+const copyedObj = Object.assign(obj2,obj1);
+
+console.log(obj1);
+console.log(obj2);
+console.log(copyedObj);
+
+
