b88e639cf6983163907b9e56c687f8dfc7c7c47e
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-29 21:47:05 +0500

    Practice

--- Object Methods/Deepcopyandshallowcopy.js
+++ Object Methods/Deepcopyandshallowcopy.js
@@ -1,9 +1,15 @@
 const fuirtsArr1 = ["Apple", " Mango" , "Banana"];
 const fuirtsArr2 = fuirtsArr1;
 
-console.log(fuirtsArr1);
+// console.log(fuirtsArr1);
 
 fuirtsArr2.push("Peach");
 
-console.log(fuirtsArr2);
+// console.log(fuirtsArr2);
 
+
+const userName1 = "Misam";
+
+let userName2 = userName1;
+
+ 
