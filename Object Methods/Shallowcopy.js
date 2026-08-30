1a7c7d3b2c7edf622cbe2a5b9f1e7bf68600164b
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-31 00:46:48 +0500

    This all About Shllow Copy

--- Object Methods/Shallowcopy.js
+++ Object Methods/Shallowcopy.js
@@ -4,6 +4,24 @@ const obj1  = {
     age: 23,
 };
 
-const firstCopy = {...obj1};
-firstCopy.city = "Sahiwal"; 
-console.log(firstCopy);
+// const firstCopy = {...obj1};
+// firstCopy.city = "Sahiwal"; 
+// console.log(firstCopy);
+
+
+const obj2={
+     name: "Misam Shaban", 
+    age: 23,
+    address:{
+        city: "Sahiwal",
+    },
+}
+
+console.log(obj2.address);
+
+const secendCopy = {...obj2};
+secendCopy.fatherName = "Shaban";
+console.log(obj2);
+
+secendCopy.address.study = "BSCS";
+console.log(secendCopy);
