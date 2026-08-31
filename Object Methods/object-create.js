76916883c4d8c580831690d3ceefb38c01511a02
Misam-Shaban <maysamshaban789@gmail.com>
2026-08-31 17:27:26 +0500

    Practice

--- Object Methods/object-create.js
+++ Object Methods/object-create.js
@@ -3,3 +3,11 @@ const person= {
         console.log("Hello..");
     }
 };
+
+const student = Object.create(person);
+// const student = Object.create(null);
+
+
+console.log("Prototype",student);
+
+
