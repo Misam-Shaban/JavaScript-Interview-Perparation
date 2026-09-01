578fcb567fe4014d0191c44246d4311a4c7029b2
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-02 00:06:06 +0500

    use of get

--- Object Methods/object-defineProperty.js
+++ Object Methods/object-defineProperty.js
@@ -1,16 +1,30 @@
-const student = {
-    greet(){
-        console.log("Hello...");
-    }
+// const student = {
+//     greet(){
+//         console.log("Hello...");
+//     }
+// };
+
+// Object.defineProperty(student, "name", {
+//   value: "Misam",
+//   writable: true,
+//   enumerable: true,
+//   configurable: true
+// });
+
+// console.log(student);
+
+
+
+const user ={
+     fname: "Misam",
+     lname: "Shaban"
 };
 
-Object.defineProperty(student, "name", {
-  value: "Misam",
-  writable: true,
-  enumerable: true,
-  configurable: true
+Object.defineProperty(user,"fullname", {
+get(){
+    return `${user.fname} ${user.lname}`;    
+}
 });
 
-console.log(student);
-
+console.log(user.fullname);
 
