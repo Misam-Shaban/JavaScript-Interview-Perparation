a96f88cc1dbcf6dc2957942f6652411fd99503f4
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-03 00:51:18 +0500

    Practice

--- Object Methods/getOwnPropertyNames.js
+++ Object Methods/getOwnPropertyNames.js
@@ -11,3 +11,32 @@ Object.defineProperty(person, "secret", {
 
 console.log(Object.keys(person));
 console.log(Object.getOwnPropertyNames(person));
+
+
+
+// Practice code 
+
+const person = {
+  fname: "Misam",
+  lname: "Shaban"
+};
+
+Object.defineProperties(person, {
+  age: {
+    value: 23,
+    writable: true,
+    enumerable: true,
+    configurable: true
+  },
+
+  secret: {
+    value: "12345",
+    writable: false,
+    enumerable: false,
+    configurable: false
+  }
+});
+
+console.log(Object.keys(person));
+console.log(Object.getOwnPropertyNames(person));
+console.log(Object.getOwnPropertyDescriptors(person));
