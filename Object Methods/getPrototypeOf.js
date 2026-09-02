0ebdabf3573e8a7a1cdd3cd8abe432defa0cf995
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-03 02:28:53 +0500

    practice

--- Object Methods/getPrototypeOf.js
+++ Object Methods/getPrototypeOf.js
@@ -30,8 +30,9 @@ console.log(Object.getPrototypeOf(animal) === Object.prototype);  // ture
 
 console.log(Object.getPrototypeOf(Object.prototype) === null); // ture
 
-console.log(Object.getPrototypeOf(puppy)); // Prototype chain jis ma eat 
-// b ha isi liya eat b cahl raha ha ku puppy eat ko prototype sy inherit kr raha ha 
+console.log(Object.getPrototypeOf(puppy)); 
+// Prototype chain jis ma eat b ha isi liya eat b cahl raha ha ku
+// puppy eat ko prototype sy inherit kr raha ha 
 
 
 
