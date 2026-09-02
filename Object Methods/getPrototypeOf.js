506771f24faf3ea7ab23e56c63a42c392ae1fb8e
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-03 01:47:43 +0500

    Practice

--- Object Methods/getPrototypeOf.js
+++ Object Methods/getPrototypeOf.js
@@ -1,9 +1,40 @@
-const person = {
-  greet() {
-    console.log("Hello");
+// const person = {
+//   greet() {
+//     console.log("Hello");
+//   }
+// };
+
+// const student = Object.create(person);
+
+// console.log(Object.getPrototypeOf(student));
+
+
+const animal = {
+  eat() {
+    console.log("Eating...");
   }
 };
 
-const student = Object.create(person);
+const dog = Object.create(animal);
 
-console.log(Object.getPrototypeOf(student));
+const puppy = Object.create(dog);
+
+puppy.name = "Tommy";
+
+
+console.log(Object.getPrototypeOf(puppy) === dog); // ture 
+
+console.log(Object.getPrototypeOf(dog) === animal); // ture 
+
+console.log(Object.getPrototypeOf(animal) === Object.prototype);  // ture
+
+console.log(Object.getPrototypeOf(Object.prototype) === null); // ture
+
+console.log(Object.getPrototypeOf(puppy)); // Prototype chain jis ma eat 
+// b ha isi liya eat b cahl raha ha ku puppy eat ko prototype sy inherit kr raha ha 
+
+
+
+
+
+
