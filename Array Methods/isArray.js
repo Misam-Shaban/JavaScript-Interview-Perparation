72f678a37347e343049642f8e3042e9576a670f3
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 03:24:54 +0500

    practice

--- Array Methods/isArray.js
+++ Array Methods/isArray.js
@@ -11,3 +11,16 @@ const c = "Hello";
 const d = 100;
 
 const e = [[1, 2], [3, 4]];
+
+console.log(Array.isArray(a)); //ture
+console.log(Array.isArray(b)); // false
+console.log(Array.isArray(c)); // false
+console.log(Array.isArray(d)); // false
+console.log(Array.isArray(e)); // ture
+console.log(Array.isArray(e[0])); // ture
+
+console.log(typeof a); // object
+
+console.log(Array.isArray(a)); // ture
+
+// isArray() Check krta ha Array ha ya nh ha 
