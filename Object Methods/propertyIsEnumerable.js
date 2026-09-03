87baf2d690f164d205cfd75530ab0f445a667f80
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 01:15:01 +0500

    To String

--- Object Methods/propertyIsEnumerable.js
+++ Object Methods/propertyIsEnumerable.js
@@ -1,2 +1,13 @@
-// propertyIsEnumerable() check karta hai ke koi 
-// own property enumerable hai ya nahi
+ // propertyIsEnumerable() check karta hai ke koi 
+ // own property enumerable hai ya nahi
+
+
+// Object.keys()
+// → enumerable own string keys ko return karta hai
+
+// propertyIsEnumerable()
+// → ek specific property ke enumerable hone ko check karta hai
+
+
+// propertyIsEnumerable() sirf own property ke liye true dega
+
