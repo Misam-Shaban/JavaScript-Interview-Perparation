71680e2ba66a0708b9e29e1ee8bd4bfd9487e720
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 00:09:25 +0500

    Practice

--- Object Methods/isPrototypeOf().js
+++ Object Methods/isPrototypeOf().js
@@ -1,2 +1,13 @@
 // isPrototypeOf() check karta hai ke kya ek object doosre 
 //object ki prototype chain mein mojood hai.
+
+
+const person = {
+    name: "Misam Shaban",
+    city: "Sahiwal",
+};
+
+const student = Object.create(person);
+
+console.log(person.isPrototypeOf(student));
+
