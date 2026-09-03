37525fb815d391a3a166067101d1c295edc82325
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 01:26:35 +0500

    toLocaleString

---
+++ Object Methods/toLocaleString.js
@@ -0,0 +1,7 @@
+// Simple definition:
+
+// toLocaleString() kisi value ko user's locale/language/region 
+// ke rules ke mutabiq string representation mein convert karta hai.
+
+// Sabse important baat: iska exact output object/value ke
+//  type aur locale par depend karta hai.
