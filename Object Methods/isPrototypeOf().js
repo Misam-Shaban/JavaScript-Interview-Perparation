10df2790ee23220038f5f41caeb35f009f651eb1
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 00:54:10 +0500

    Practice

--- Object Methods/isPrototypeOf().js
+++ Object Methods/isPrototypeOf().js
@@ -5,9 +5,24 @@
 const person = {
     name: "Misam Shaban",
     city: "Sahiwal",
+    address:{
+        add: "cotton Research Station Sahiwal",
+    },
 };
 
 const student = Object.create(person);
 
 console.log(person.isPrototypeOf(student));
+
+
+// getPrototypeOf()
+// → mujhe direct prototype do
+
+// isPrototypeOf()
+// → kya ye object doosre ki prototype chain mein hai?
+
+
+
+
+
 
