a3c1080abd591ace2ec212738558c455ae49b02f
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 01:25:08 +0500

    Practice

--- Object Methods/toString.js
+++ Object Methods/toString.js
@@ -1,5 +1,23 @@
+// const user = {
+//   name: "Misam"
+// };
+
+// console.log(Object.prototype.toString.call(user));
+
+
 const user = {
   name: "Misam"
 };
 
-console.log(Object.prototype.toString.call(user));
+console.log(user.toString());
+console.log(user.hasOwnProperty("toString"));
+
+console.log("toString" in user);
+console.log(
+  Object.prototype.toString.call(user)
+);
+
+console.log(Object.prototype.toString.call([]));
+console.log(Object.prototype.toString.call({}));
+console.log(Object.prototype.toString.call(null));
+console.log(Object.prototype.toString.call(undefined));
