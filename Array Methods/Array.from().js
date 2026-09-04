f4d88b406b90133227034d245b6fc948ff30bbfe
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 15:29:41 +0500

    JSON

--- Array Methods/Array.from().js
+++ Array Methods/Array.from().js
@@ -1,3 +1,11 @@
 // Definition
 
 // Array.from() kisi iterable ya array-like object se ek new Array create karta hai.
+
+
+const text = "Hello";
+
+const result = Array.from(text);
+
+console.log(result);
+
