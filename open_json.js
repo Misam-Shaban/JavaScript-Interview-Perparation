da1e634be0826e683bedb9b2c84f602ee086efdb
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-04 16:19:30 +0500

    Run Json

--- open_json.js
+++ open_json.js
@@ -1,7 +1,12 @@
-{
+const json = 
+    `{
     "name":"Misam shaban",
     "city":"Sahiwal",
     "score":12345,
     "isAdmin": false
-}
+}`;
 
+
+const user =JSON.parse(json);
+
+console.log(user);
