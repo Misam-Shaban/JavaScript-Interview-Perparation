e7fd53158b18192e9fbc5c9b6a257082f032d9dd
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-08 03:46:16 +0500

    Preactice

--- Array Methods/Array.from().js
+++ Array Methods/Array.from().js
@@ -8,11 +8,25 @@
 
 // console.log(result);
 
-const obj = {
-  name: "Misam",
-  func: () => {
-    console.log(this.name);
-  },
-};
+// const obj = {
+//   name: "Misam",
+//   func: () => {
+//     console.log(this.name);
+//   },
+// };
 
 // obj.func();
+
+// const str = "12345";
+// const num = "12345";
+
+// const result = Array.from(num);
+// console.log(result);
+
+// console.log(typeof result);
+
+const names = ["Ali", "Ahmed", "Sara"];
+
+const result = Array.from(names, (value, index) => `${index} ${value}`);
+console.log(typeof result);
+console.log(result);
