6d3d2f078bb9a3d9db876943b2a925442b7aecee
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-08 04:04:39 +0500

    Practice

--- Array Methods/Array.from().js
+++ Array Methods/Array.from().js
@@ -25,8 +25,42 @@
 
 // console.log(typeof result);
 
-const names = ["Ali", "Ahmed", "Sara"];
+// const names = ["Ali", "Ahmed", "Sara"];
 
-const result = Array.from(names, (value, index) => `${index} ${value}`);
-console.log(typeof result);
-console.log(result);
+// const result = Array.from(names, (value, index) => `${index} ${value}`);
+// console.log(typeof result);
+// console.log(result);
+
+// const text = "HELLO";
+// console.log(typeof text);
+// const newText = Array.from(text);
+// console.log(newText);
+// console.log(typeof newText);
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const multi = Array.from(numbers, (num) => num ** 2); // ** Operator ko hum Square k liya use krty ha
+// console.log(multi);
+
+// const obj = {
+//   0: 10,
+//   1: 20,
+//   2: 30,
+//   length: 3,
+// };
+
+// const makeArr = Array.from(obj);
+
+// console.log(makeArr);
+
+// const names = ["Ali", "Ahmed", "Sara"];
+
+// const result = Array.from(names, (value, index) => `${index} ${value}`);
+
+// const arr = [1, 2, 3];
+
+// const result = Array.from(arr);
+
+// console.log(result === arr);
+
+// false;
