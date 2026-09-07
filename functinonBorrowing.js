87396e767eac67418e2468f88ccb016dd40eebd7
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-07 17:17:35 +0500

    Practice

--- functinonBorrowing.js
+++ functinonBorrowing.js
@@ -6,3 +6,11 @@ const person1 = {
 };
 
 person1.greet(); // "Hi, I am Ali" ✅ theek chal raha hai
+
+const person2 = {
+  name: "Sara",
+  // greet function nahi hai is object mein
+};
+
+// person1 ka greet function, person2 ke liye "borrow" kar rahe hain
+person1.greet.call(person2); // "Hi, I am Sara"
