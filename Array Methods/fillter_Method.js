b3c5fc353dccccbc23ace679be29a1e08df8dc9d
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-20 18:40:16 +0500

    Master

--- Array Methods/fillter_Method.js
+++ Array Methods/fillter_Method.js
@@ -33,16 +33,44 @@
 
 // console.log(result);
 
-const students = [
-  { name: "Ali", marks: 80 },
-  { name: "Ahmed", marks: 55 },
-  { name: "Sara", marks: 90 },
-  { name: "Usman", marks: 65 },
-  { name: "Ayesha", marks: 75 },
+// const students = [
+//   { name: "Ali", marks: 80 },
+//   { name: "Ahmed", marks: 55 },
+//   { name: "Sara", marks: 90 },
+//   { name: "Usman", marks: 65 },
+//   { name: "Ayesha", marks: 75 },
+// ];
+
+// const std = students.filter((stdMark) => {
+//   return stdMark.marks > 70;
+// });
+
+// console.log(std);
+
+// const users = [
+//   { name: "Ali", age: 22, active: true },
+//   { name: "Ahmed", age: 17, active: true },
+//   { name: "Sara", age: 25, active: false },
+//   { name: "Usman", age: 30, active: true },
+//   { name: "Ayesha", age: 19, active: false },
+// ];
+
+// const result = users.filter((stdData) => {
+//   return stdData.age >= 18 && stdData.active === true;
+// });
+
+// console.log(result);
+
+const products = [
+  { name: "Laptop", price: 100000, stock: 5 },
+  { name: "Mouse", price: 2000, stock: 0 },
+  { name: "Keyboard", price: 5000, stock: 10 },
+  { name: "Monitor", price: 30000, stock: 3 },
+  { name: "Headphones", price: 8000, stock: 0 },
 ];
 
-const std = students.filter((stdMark) => {
-  return stdMark.marks > 70;
+const res = products.filter((find) => {
+  return find.price >= 5000 && find.stock > 0;
 });
 
-console.log(std);
+console.log(res);
