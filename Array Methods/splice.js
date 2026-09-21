aa68633cd1043c66a9ad622915c40bdd00c32f72
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 16:26:13 +0500

    Splice

---
+++ Array Methods/splice.js
@@ -0,0 +1,6 @@
+// splice() kya karta hai?
+
+// splice() array ke andar se elements ko remove, add, ya replace karta hai.
+
+// slice()  → copy nikalta hai, original array ko change nahi karta
+// splice() → original array ko change karta hai
