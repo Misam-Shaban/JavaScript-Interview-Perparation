e8e7363ec80c1a4fe6e4592db0e88d2d5a6cb251
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 19:18:13 +0500

    Master

--- Array Methods/include.js
+++ Array Methods/include.js
@@ -7,7 +7,12 @@
 // console.log(fruits.includes("Mango"));
 // console.log(fruits.includes("Grapes"));
 
-const numbers = [10, 20, 30, 40, 20, 50, 30, 70, 80];
+// const numbers = [10, 20, 30, 40, 20, 50, 30, 70, 80];
 
-console.log(numbers.includes(20)); // ture
-console.log(numbers.includes(20, 5)); // false ku k humny check krna ha index 5 sy start kiya ha
+// console.log(numbers.includes(20)); // ture
+// console.log(numbers.includes(20, 5)); // false ku k humny check krna ha index 5 sy start kiya ha
+
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers.indexOf(30));
+console.log(numbers.indexOf(100));
