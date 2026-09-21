e32629b247da204968913d7f75a52b18968f1dfa
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 16:12:32 +0500

    Slice

---
+++ Array Methods/slice.js
@@ -0,0 +1,11 @@
+const numbers = [10, 20, 30, 40, 50, 60];
+
+const result = numbers.slice(-3);
+
+console.log(result);
+
+// const fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+// const result = fruits.slice(1);
+
+// console.log(result);
