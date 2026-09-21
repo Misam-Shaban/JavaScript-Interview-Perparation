1bc1dfa76d0e8103129c894fefaa0c65fa63e193
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 16:22:52 +0500

    Master Slice

--- Array Methods/slice.js
+++ Array Methods/slice.js
@@ -1,11 +1,27 @@
-const numbers = [10, 20, 30, 40, 50, 60];
+// const numbers = [10, 20, 30, 40, 50, 60];
 
-const result = numbers.slice(-3);
+// const result = numbers.slice(-3);
 
-console.log(result);
+// console.log(result);
+
+// const numbers = [10, 20, 30, 40, 50, 60];
+
+// const result = numbers.slice(2,5);
+
+// console.log(result);
 
 // const fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
 
 // const result = fruits.slice(1);
 
 // console.log(result);
+
+// slice(start, end)  → specific portion
+// slice(start)       → start se end tak
+// slice(-n)           → last n items
+
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80];
+
+const result = numbers.slice(2, 6);
+
+console.log(result);
