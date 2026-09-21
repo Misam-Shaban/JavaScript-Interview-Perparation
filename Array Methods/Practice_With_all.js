3ec88f863adf75041d48c65a6a821bca22cdb256
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 17:13:43 +0500

    Practice

--- Array Methods/Practice_With_all.js
+++ Array Methods/Practice_With_all.js
@@ -1,7 +1,26 @@
-const numbers = [10, 15, 20, 25, 30, 35];
+// Use of Fillter Method
+// const numbers = [10, 15, 20, 25, 30, 35];
 
-const result = numbers.find((data) => {
-  return data > 25;
+// const result = numbers.filter((data) => {
+//   return data > 20;
+// });
+
+// console.log(result);
+
+// Use of Map Method
+
+// const numbers = [5, 10, 15, 20, 25, 30];
+
+// const result = numbers.map((elem) => {
+//   return elem * 2;
+// });
+
+// console.log(result);
+
+const numbers = [5, 12, 18, 25, 30];
+
+const result = numbers.find((elem) => {
+  return elem > 20;
 });
 
 console.log(result);
