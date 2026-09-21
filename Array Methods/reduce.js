58e332ff2698fb673ba34a30487edc01493dc39e
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 15:58:37 +0500

    Practice

--- Array Methods/reduce.js
+++ Array Methods/reduce.js
@@ -1,7 +1,46 @@
-const numbers = [5, 10, 15];
+// const numbers = [2, 4, 6, 8];
+// const total = numbers.reduce((acc, num) => {
+//   return acc * num;
+// }, 1);
 
-const total = numbers.reduce((acc, num) => {
-  return acc + num;
-}, 0);
+// console.log(total);
 
-console.log(total);
+// const products = [
+//   { name: "Laptop", price: 100000 },
+//   { name: "Mouse", price: 2000 },
+//   { name: "Keyboard", price: 5000 },
+//   { name: "Monitor", price: 30000 },
+// ];
+
+// const total = products.reduce((acc, num) => {
+//   if (num.price > 5000) {
+//     return acc + num.price;
+//   }
+
+//   return acc;
+// }, 0);
+
+// console.log(total);
+
+// const products = [
+//   { name: "Laptop", price: 100000 },
+//   { name: "Mouse", price: 2000 },
+//   { name: "Keyboard", price: 5000 },
+//   { name: "Monitor", price: 30000 },
+// ];
+
+// const total = products.reduce((acc, prices) => {
+//   if (prices.price > 10000) {
+//     return acc + prices.price;
+//   }
+//   return acc;
+// }, 0);
+// console.log(total);
+
+// const students = ["Ali", "Ahmed", "Sara", "Usman", "Ayesha"];
+
+// const totalStuent = students.reduce((acc, num) => {
+//   return acc + 1;
+// }, 0);
+
+// console.log("Total Students", totalStuent);
