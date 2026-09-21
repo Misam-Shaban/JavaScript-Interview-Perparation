9709730d234c2d5bf63a6fdcd09ee00716d0c2f6
Misam-Shaban <maysamshaban789@gmail.com>
2026-09-21 16:48:42 +0500

    master

--- Array Methods/splice.js
+++ Array Methods/splice.js
@@ -4,3 +4,37 @@
 
 // slice()  → copy nikalta hai, original array ko change nahi karta
 // splice() → original array ko change karta hai
+
+// // Practice 1
+// const numbers = [10, 20, 30, 40, 50];
+
+// const result = numbers.splice(2, 1);
+
+// console.log(result); // [30]
+
+// // Updated Array
+// console.log(numbers); // [10, 20, 40, 50]
+
+// Practice for Addition
+// const numbers = [10, 20, 50, 60];
+
+// const addition = numbers.splice(2, 0, 30, 40);
+
+// // console.log(addition);
+// console.log(numbers);
+
+// // Practice for Replace
+
+// const numbers = [10, 20, 30, 40, 50];
+
+// const replce = numbers.splice(2, 1, 300);
+
+// console.log(replce); // [30]
+// console.log(numbers); // [ 10, 20, 300, 40, 50 ]
+
+// Final Practice
+const students = ["Ali", "Ahmed", "Sara", "Usman", "Ayesha"];
+
+students.splice(2, 1, "Hina", "Fatima");
+
+console.log(students);
